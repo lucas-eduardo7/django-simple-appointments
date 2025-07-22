bc0a5c7ec24063cbb814203220fba807693589c6
lucas-eduardo7 <lucaseduardo.s07@gmail.com>
2025-07-22 07:21:51 -0300

    Add 'providers' and 'recipients' fields to Appointment model with ManyToMany through AppointmentProvider and AppointmentRecipient

--- appointments/models.py
+++ appointments/models.py
@@ -1,6 +1,29 @@
 from django.db import models
+from .conf import get_setting
 
 
 class Appointment(models.Model):
-    provider = None
-    recipient = None
+    providers = models.ManyToManyField(
+        to=get_setting("APPOINTMENTS_PROVIDER_MODEL"),
+        through="AppointmentProvider",
+        related_name="appointments_as_provider",
+    )
+    recipients = models.ManyToManyField(
+        to=get_setting("APPOINTMENTS_RECIPIENT_MODEL"),
+        through="AppointmentRecipient",
+        related_name="appointments_as_recipient",
+    )
+
+
+class AppointmentProvider(models.Model):
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    provider = models.ForeignKey(
+        to=get_setting("APPOINTMENTS_PROVIDER_MODEL"), on_delete=models.CASCADE
+    )
+
+
+class AppointmentRecipient(models.Model):
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    provider = models.ForeignKey(
+        to=get_setting("APPOINTMENTS_RECIPIENT_MODEL"), on_delete=models.CASCADE
+    )
