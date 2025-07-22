a20337b60874d6eee77c217913b8ee463807aaa7
lucas-eduardo7 <lucaseduardo.s07@gmail.com>
2025-07-22 11:01:02 -0300

    add appointment register in admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Appointment, AppointmentProvider, AppointmentRecipient
 
-# Register your models here.
+
+class AppointmentProviderInline(admin.TabularInline):
+    model = AppointmentProvider
+    extra = 1
+
+
+class AppointmentRecipientInline(admin.TabularInline):
+    model = AppointmentRecipient
+    extra = 1
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    inlines = [AppointmentProviderInline, AppointmentRecipientInline]
