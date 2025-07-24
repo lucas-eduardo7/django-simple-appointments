5a3681ab49377092759b4e9b4fa25dff0640938b
lucas-eduardo7 <lucaseduardo.s07@gmail.com>
2025-07-24 14:25:06 -0300

    add date validation

--- appointments/models.py
+++ appointments/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from .conf import get_setting
-from .utils import check_appointments_conflicts
+from .utils import validate_appointments_conflicts, validate_time_cohesion
 
 
 class Appointment(models.Model):
@@ -15,17 +15,21 @@ class Appointment(models.Model):
         through="AppointmentRecipient",
         related_name="appointments_as_recipient",
     )
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    activities = models.ManyToManyField(
+        to=get_setting("APPOINTMENTS_ACTIVITIES_MODEL"),
+        related_name="appointments_as_activities",
+    )
     status = models.CharField(
         max_length=30,
         choices=get_setting("APPOINTMENTS_STATUS_CHOICES"),
         default="pending",
         blank=True,
     )
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     is_blocked = models.BooleanField(default=False)
-    block_reason = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
     def clean(self):
         super().clean()
@@ -34,7 +38,11 @@ class Appointment(models.Model):
             return
 
         for provider in self.providers.all():
-            conflict_message = check_appointments_conflicts(self, provider)
+            time_cohesion = validate_time_cohesion(self.start_time, self.end_time)
+            conflict_message = validate_appointments_conflicts(self, provider)
+
+            if time_cohesion:
+                raise ValidationError(time_cohesion)
             if conflict_message:
                 raise ValidationError(conflict_message)
 
@@ -52,7 +60,9 @@ class AppointmentProvider(models.Model):
     def clean(self):
         super().clean()
 
-        conflict_message = check_appointments_conflicts(self.appointment, self.provider)
+        conflict_message = validate_appointments_conflicts(
+            self.appointment, self.provider
+        )
         if conflict_message:
             raise ValidationError(conflict_message)
 
