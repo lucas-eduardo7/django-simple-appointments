ecab9e2179e711568de25e63d2f03b71467ef63e
lucas-eduardo7 <lucaseduardo.s07@gmail.com>
2025-08-21 17:17:35 -0300

    remove activities app

--- activities/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Activity
-
-
-@admin.register(Activity)
-class ActivityAdmin(admin.ModelAdmin):
-    list_display = ("id", "price", "duration_time")
