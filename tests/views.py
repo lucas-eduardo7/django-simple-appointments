1d88d97b57d9c4ee14738bb41c4f870561f59f0e
lucas-eduardo7 <lucaseduardo.s07@gmail.com>
2025-08-21 16:22:02 -0300

    adds test app for testing

---
+++ tests/views.py
@@ -0,0 +1,14 @@
+from appointments.views import FormWizardView
+from django.shortcuts import render
+from django.views import View
+
+
+class TestFormWizardView(FormWizardView):
+    template_name = "home.html"
+    next_url = "wizard"
+    success_url = "wizard_success"
+
+
+class TestSucessWizardView(View):
+    def get(self, request):
+        return render(request, "home.html")
