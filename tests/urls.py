96b78d0335d158fa9835335538efd2d559b40dd6
lucas-eduardo7 <lucaseduardo.s07@gmail.com>
2025-08-22 18:51:35 -0300

    remove testes/templates/, tests/urls, testes/views

--- tests/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from tests.views import TestFormWizardView, TestSucessWizardView
-
-urlpatterns = [
-    path("wizard/<int:step>/", TestFormWizardView.as_view(), name="wizard"),
-    path("success/", TestSucessWizardView.as_view(), name="wizard_success"),
-]
